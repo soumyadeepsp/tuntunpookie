8e48209cc09704a375b1703fbc896033e00b0a9e
soupaul_expedia <soupaul@expediagroup.com>
2025-03-06 01:23:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
